06d875df2ef36bc5ad3549385aacd65a1dd103d1
luizavpg-bit <luizavpg@gmail.com>
2026-08-31 21:40:26 -0300

    finaliza projeto

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,36 +1,37 @@
+import ListaPostagens from "../../components/postagem/listapostagens/ListaPostagens"
+import ModalPostagem from "../../components/postagem/modalpostagem/ModalPostagem";
+
 function Home() {
-    return (
-        <>
-            <div className="bg-indigo-900 flex justify-center min-h-[80vh]">
-                <div className='container grid grid-cols-1 md:grid-cols-2 text-white'>
-                    <div className="flex flex-col gap-4 items-center justify-center py-4">
-                        <h2 className='text-5xl font-bold'>
-                            Seja Bem Vinde!
-                        </h2>
-                        <p className='text-xl'>
-                            Expresse aqui seus pensamentos e opniões
-                        </p>
+	return (
+		<>
+			<div className="bg-indigo-900 flex justify-center">
+				{/* Seção com 2 colunas */}
+				<div className="container grid grid-cols-1 md:grid-cols-2 text-white">
+					{/* Conteúdo de texto */}
+					<div className="flex flex-col gap-4 items-center justify-center py-4">
+						<h2 className="text-2xl md:text-5xl font-bold">Seja Bem Vinde!</h2>
 
-                        <div className="flex justify-around gap-4">
-                            <div className='rounded text-white 
-                                            border-white border-solid border-2 py-2 px-4'
-                                >
-                                Nova Postagem
-                            </div>
-                        </div>
-                    </div>
+						<p className="text-xl">Expresse aqui seus pensamentos</p>
 
-                    <div className="flex justify-center ">
-                        <img
-                            src="https://i.imgur.com/fyfri1v.png"
-                            alt="Imagem Página Home"
-                            className='w-2/3'
-                        />
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+						<div className="flex justify-around gap-4">
+							<ModalPostagem />
+						</div>
+					</div>
+
+					{/* Imagem da página home */}
+					<div className="flex justify-center">
+						<img
+							src="https://i.imgur.com/fyfri1v.png"
+							alt="Imagem da página Home"
+							className="w-2/3"
+						/>
+					</div>
+				</div>
+			</div>
+
+			<ListaPostagens />
+		</>
+	)
 }
 
 export default Home
